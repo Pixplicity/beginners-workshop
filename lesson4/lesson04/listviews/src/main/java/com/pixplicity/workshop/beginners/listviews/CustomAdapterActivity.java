500a629dd161b654444cb1d440a73cf411365534
Paul Lammertsma <pflammertsma@gmail.com>
2016-02-24 16:48:23 +0100

    moved packages

---
+++ lesson4/lesson04/listviews/src/main/java/com/pixplicity/workshop/beginners/listviews/CustomAdapterActivity.java
@@ -0,0 +1,29 @@
+package com.pixplicity.workshop.beginners.listviews;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+public class CustomAdapterActivity extends ListActivity {
+
+    private CustomAdapter mCustomAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCustomAdapter = new CustomAdapter(this);
+        getListView().setAdapter(mCustomAdapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        // TODO Complete the `onListItemClick` method. Use the `position` to retrieve an `Animal` object from the `mCustomerAdapter` field and use the `animal.infoUrl` information to create a valid `Uri`
+        Uri uri = null;
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+}
