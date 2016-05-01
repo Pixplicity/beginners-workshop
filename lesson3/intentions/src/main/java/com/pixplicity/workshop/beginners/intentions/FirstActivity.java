33fdac8bb46d5ee7a4ee185265d6db8f83223a35
Paul Lammertsma <pflammertsma@gmail.com>
2016-05-01 18:15:25 +0200

    solutions for lesson 3

--- lesson3/intentions/src/main/java/com/pixplicity/workshop/beginners/intentions/FirstActivity.java
+++ lesson3/intentions/src/main/java/com/pixplicity/workshop/beginners/intentions/FirstActivity.java
@@ -1,6 +1,7 @@
 package com.pixplicity.workshop.beginners.intentions;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,7 +30,16 @@ public class FirstActivity extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-        // TODO Exercise 3.1
+        // Exercise 3.1
+
+        // Beware: we need to make sure to convert the text to a String!
+        String emailAddress = mEmailAddress.getText().toString();
+        String emailSubject = mEmailSubject.getText().toString();
+
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra(SecondActivity.EXTRA_EMAIL_ADDRESS, emailAddress);
+        intent.putExtra(SecondActivity.EXTRA_EMAIL_SUBJECT, emailSubject);
+        startActivity(intent);
     }
 
 }
