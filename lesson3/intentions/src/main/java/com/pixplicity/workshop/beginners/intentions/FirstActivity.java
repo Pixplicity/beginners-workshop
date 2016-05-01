9f710062fe5017f2e7cc96508568e1bda762427c
Paul Lammertsma <pflammertsma@gmail.com>
2016-05-01 19:10:32 +0200

    solution for lesson 3, exercise 3.4

--- lesson3/intentions/src/main/java/com/pixplicity/workshop/beginners/intentions/FirstActivity.java
+++ lesson3/intentions/src/main/java/com/pixplicity/workshop/beginners/intentions/FirstActivity.java
@@ -2,16 +2,21 @@ package com.pixplicity.workshop.beginners.intentions;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class FirstActivity extends Activity implements View.OnClickListener {
+public class FirstActivity extends Activity {
+
+    public static final int REQUEST_CODE = 12345;
 
     private EditText mEmailAddress;
 
     private EditText mEmailSubject;
+
+    private EditText mEmailMessage;
 
     private Button mButton;
 
@@ -23,23 +28,50 @@ public class FirstActivity extends Activity implements View.OnClickListener {
         mEmailAddress = (EditText) findViewById(R.id.email_address);
         mEmailSubject = (EditText) findViewById(R.id.email_subject);
 
+        mEmailMessage = (EditText) findViewById(R.id.email_message);
+        mEmailMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Beware: we need to make sure to convert the text to a String!
+                String emailMessage = mEmailMessage.getText().toString();
+
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                intent.putExtra(SecondActivity.EXTRA_EMAIL_MESSAGE, emailMessage);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+
         mButton = (Button) findViewById(R.id.next);
-        mButton.setOnClickListener(this);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailAddress = mEmailAddress.getText().toString();
+                String emailSubject = mEmailSubject.getText().toString();
+                String emailMessage = mEmailMessage.getText().toString();
+
+                Intent intent = new Intent(
+                        Intent.ACTION_SENDTO,
+                        Uri.parse("mailto:" + emailAddress));
+                intent.putExtra(Intent.EXTRA_SUBJECT,
+                        emailSubject);
+                intent.putExtra(Intent.EXTRA_TEXT,
+                        emailMessage);
+                startActivity(Intent.createChooser(intent,
+                        "Send mail…"));
+            }
+        });
 
     }
 
     @Override
-    public void onClick(View v) {
-        // Exercise 3.1
-
-        // Beware: we need to make sure to convert the text to a String!
-        String emailAddress = mEmailAddress.getText().toString();
-        String emailSubject = mEmailSubject.getText().toString();
-
-        Intent intent = new Intent(this, SecondActivity.class);
-        intent.putExtra(SecondActivity.EXTRA_EMAIL_ADDRESS, emailAddress);
-        intent.putExtra(SecondActivity.EXTRA_EMAIL_SUBJECT, emailSubject);
-        startActivity(intent);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 12345) {
+            if (resultCode == RESULT_OK && data != null) {
+                mEmailMessage.setText(data.getStringExtra("message"));
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
 }
--- lesson3/intentions/src/main/java/com/pixplicity/workshop/beginners/intentions/SecondActivity.java
+++ lesson3/intentions/src/main/java/com/pixplicity/workshop/beginners/intentions/SecondActivity.java
@@ -2,21 +2,14 @@ package com.pixplicity.workshop.beginners.intentions;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class SecondActivity extends Activity implements View.OnClickListener {
 
-    public static final String EXTRA_EMAIL_ADDRESS = "email_address";
-
-    public static final String EXTRA_EMAIL_SUBJECT = "email_subject";
-
-    private String mEmailAddress;
-    private String mEmailSubject;
+    public static final String EXTRA_EMAIL_MESSAGE = "email_message";
 
     private EditText mEmailMessage;
 
@@ -27,19 +20,12 @@ public class SecondActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        if (savedInstanceState != null) {
-            mEmailAddress = savedInstanceState.getString(EXTRA_EMAIL_ADDRESS);
-            mEmailSubject = savedInstanceState.getString(EXTRA_EMAIL_SUBJECT);
-        } else {
-            mEmailAddress = getIntent().getStringExtra(EXTRA_EMAIL_ADDRESS);
-            mEmailSubject = getIntent().getStringExtra(EXTRA_EMAIL_SUBJECT);
-        }
-
-        if (TextUtils.isEmpty(mEmailAddress) || TextUtils.isEmpty(mEmailSubject)) {
-            throw new RuntimeException("The e-mail address and e-mail subject must both be passed as String extras to the Intent");
-        }
-
         mEmailMessage = (EditText) findViewById(R.id.email_message);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            mEmailMessage.setText(intent.getStringExtra(EXTRA_EMAIL_MESSAGE));
+        }
 
         mSendButton = (Button) findViewById(R.id.send);
         mSendButton.setOnClickListener(this);
@@ -47,27 +33,11 @@ public class SecondActivity extends Activity implements View.OnClickListener {
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString(EXTRA_EMAIL_ADDRESS, mEmailAddress);
-        outState.putString(EXTRA_EMAIL_SUBJECT, mEmailSubject);
-    }
-
-    @Override
     public void onClick(View v) {
-        // Exercise 3.2
-
-        String emailMessage = mEmailMessage.getText().toString();
-
-        Intent intent = new Intent(
-                Intent.ACTION_SENDTO,
-                Uri.parse("mailto:" + mEmailAddress));
-        intent.putExtra(Intent.EXTRA_SUBJECT,
-                mEmailSubject);
-        intent.putExtra(Intent.EXTRA_TEXT,
-                emailMessage);
-        startActivity(Intent.createChooser(intent,
-                "Send mail…"));
+        Intent intent = new Intent();
+        intent.putExtra("message", mEmailMessage.getText().toString());
+        setResult(RESULT_OK, intent);
+        finish();
     }
 
 }
